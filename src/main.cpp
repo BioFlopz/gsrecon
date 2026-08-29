d358c9f2c93325eada8985d2e33010a3428030ee
en_why <yynahim@gmail.com>
2026-08-29 01:20:56 +0100

    add win32 window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,69 @@
-#include <iostream>
 
-int main()
+#include <Windows.h>
+
+
+LRESULT CALLBACK WindowProc(
+    HWND hwnd,
+    UINT message,
+    WPARAM wParam,
+    LPARAM lParam)
 {
-    std::cout << "Gaussian reconstruction skeleton\n";
-    return 0;
+    switch (message)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+
+
+int WINAPI wWinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    PWSTR pCmdLine,
+    int nCmdShow)
+{
+    const wchar_t CLASS_NAME[] = L"gsrecon window";
+
+    WNDCLASS windowClass{};
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.hInstance = hInstance;
+    windowClass.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&windowClass);
+
+	HWND window = CreateWindowEx(
+	    0,
+	    CLASS_NAME,
+	    L"gsrecon",
+	    WS_OVERLAPPEDWINDOW,
+	    CW_USEDEFAULT,
+	    CW_USEDEFAULT,
+	    CW_USEDEFAULT,
+	    CW_USEDEFAULT,
+	    nullptr,
+	    nullptr,
+	    hInstance,
+	    nullptr
+	);
+
+	if (window == nullptr)
+	{
+	    return 0;
+	}
+
+	ShowWindow(window, nCmdShow);
+
+	MSG message{};
+
+	while (GetMessage(&message, nullptr, 0, 0) > 0)
+	{
+	    TranslateMessage(&message);
+	    DispatchMessage(&message);
+	}
+
+	return 0;
 }
