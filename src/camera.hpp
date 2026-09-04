694e662a09abbe035d7665dd3505888ad0d31fd7
BioFlopz <abiodun3d@gmail.com>
2026-09-04 03:06:52 +0100

    Implement projected Gaussian footprint

--- src/camera.hpp
+++ src/camera.hpp
@@ -6,10 +6,14 @@ struct alignas(16) CameraGpuData
 {
     float view[16];
     float projection[16];
+
+    float viewportSize[2];
+    float padding[2];
 };
 
-static_assert(sizeof(CameraGpuData) == 128);
+static_assert(sizeof(CameraGpuData) == 144);
 static_assert(alignof(CameraGpuData) == 16);
 
 static_assert(offsetof(CameraGpuData, view) == 0);
 static_assert(offsetof(CameraGpuData, projection) == 64);
+static_assert(offsetof(CameraGpuData, viewportSize) == 128);
